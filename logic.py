64d8536e3a4e052c554eed6f9c398651916fa5d6
Krzysztof <64757570+Nkifor@users.noreply.github.com>
2023-07-08 17:18:53 +0200

    fixed initial commit of base logic

--- logic.py
+++ logic.py
@@ -16,13 +16,13 @@ project_name = "CNN_Cat_health_classifier"
 # research/trials is needed for experiments to keep them there
 list_of_files = [
     ".github/workflows/.gitkeep",
-    f"src{project_name}/__init__.py",
-    f"src{project_name}/components/__init__.py",
-    f"src{project_name}/utils/__init__.py",
-    f"src{project_name}/config/configuration.py",
-    f"src{project_name}/pipeline/__init__.py",
-    f"src{project_name}/entity/__init__.py",
-    f"src{project_name}/constants/__init__.py",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "dvc.yaml",
     "params.yaml",
@@ -35,6 +35,7 @@ list_of_files = [
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
+
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Created directory {filedir} for file {filename}")
