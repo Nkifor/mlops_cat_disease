fb42bdddbd01ab8ef6cb63b834eb7c5b05b88519
Krzysztof <64757570+Nkifor@users.noreply.github.com>
2023-07-12 00:39:35 +0200

    modular base model created

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from CNN_Cat_health_classifier import logger
 from CNN_Cat_health_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from CNN_Cat_health_classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -11,3 +12,16 @@ try:
 except Exception as e:
     logger.exception(f">>>>> Stage | {STAGE_NAME} | Failed")
     raise e
+
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> Stage | {STAGE_NAME} | Started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> Stage {STAGE_NAME} | Finished <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
